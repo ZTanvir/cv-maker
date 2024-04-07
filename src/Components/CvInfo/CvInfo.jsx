d3190f307a0274514bbb5459010073c287fda2c4
ztanvir <zahirulopel@gmail.com>
2024-04-07 15:46:37 +0600

    Add props to InputField component

--- src/Components/CvInfo/CvInfo.jsx
+++ src/Components/CvInfo/CvInfo.jsx
@@ -6,7 +6,19 @@
  *Skills
  form fields
  */
+import InputField from "../InputField";
+
 const CvInfo = () => {
-    return <section className="cvInfo"></section>;
+    return (
+        <section className="cvInfo">
+            <InputField
+                label="Full Name"
+                type="text"
+                placeholder="Full name"
+                name="fullName"
+                isRequired={true}
+            />
+        </section>
+    );
 };
 export default CvInfo;
--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -1,7 +1,19 @@
-const InputField = () => {
+import { useId } from "react";
+
+const InputField = (props) => {
+    const inputId = useId();
+    const { label, type, placeholder, name, isRequired } = props;
+    const uniqueId = `${type}${inputId}`;
     return (
         <div>
-            <input />
+            <label htmlFor={uniqueId}>{label}</label>
+            <input
+                id={uniqueId}
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                required={isRequired}
+            />
         </div>
     );
 };
