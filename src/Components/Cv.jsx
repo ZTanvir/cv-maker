059aa5063aab3a4662e7f7fe7f8966d72f4373de
ztanvir <zahirulopel@gmail.com>
2024-04-07 15:17:32 +0600

    Add CvInfo component to get user cv data

--- src/Components/Cv.jsx
+++ src/Components/Cv.jsx
@@ -1,7 +1,12 @@
 // In front user will see both form field which will use to get their user information
 // and how the information will look in the cv
+import CvInfo from "./CvInfo/CvInfo";
 
 const Cv = () => {
-    return <section className="cv-maker"></section>;
+    return (
+        <section className="cv-maker">
+            <CvInfo />
+        </section>
+    );
 };
 export default Cv;
---
+++ src/Components/CvInfo/CvInfo.jsx
@@ -0,0 +1,12 @@
+/* In front user will see
+ *Personal Details
+ *Education
+ *Professional Experience
+ *Contract
+ *Skills
+ form fields
+ */
+const CvInfo = () => {
+    return <section className="cvInfo"></section>;
+};
+export default CvInfo;
