4392899715779cf75da2bc505f395673891e0270
error <ztanvir>
2024-04-09 16:15:32 +0600

    Add toggle reuseable component

---
+++ src/Components/CvInfo/UserInfo/ToggleSection.jsx
@@ -0,0 +1,5 @@
+// This is a reuseable component which task is to
+// Show or hide section based on user click
+
+const ToggleSection = () => {};
+export default ToggleSection;
---
+++ src/Components/CvInfo/UserInfo/UserInfo.jsx
@@ -0,0 +1,4 @@
+// This is a reuseable component which task is to
+
+const UserInfo = () => {};
+export default UserInfo;
