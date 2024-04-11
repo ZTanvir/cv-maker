79e63f35251640e4736bd9abbb2ef276b7a866c5
error <ztanvir>
2024-04-11 10:50:23 +0600

    Add configuration for google icon

--- src/Components/CvInfo/UserInfo/ToggleSection.jsx
+++ src/Components/CvInfo/UserInfo/ToggleSection.jsx
@@ -1,5 +1,19 @@
 // This is a reuseable component which task is to
 // Show or hide section based on user click
 
-const ToggleSection = () => {};
+const ToggleSection = (props) => {
+    const { children } = props;
+    const handleToggleSection = (event) => {};
+    return (
+        <div onClick={handleToggleSection}>
+            <div>
+                <span>
+                    <span></span> {sectionName}
+                </span>
+                <span></span>
+            </div>
+            <div>{children}</div>
+        </div>
+    );
+};
 export default ToggleSection;
