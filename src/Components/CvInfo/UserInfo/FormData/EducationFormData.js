81fe9ee0fde0591031681f0ba702eb134ab2725d
error <ztanvir>
2024-04-16 16:13:01 +0600

    Create EducationFormData to generate Education form

---
+++ src/Components/CvInfo/UserInfo/FormData/EducationFormData.js
@@ -0,0 +1,44 @@
+const EducationFormData = [
+    {
+        id: crypto.randomUUID(),
+        label: "School",
+        type: "text",
+        name: "school",
+        placeholder: "Enter school / university",
+        require: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        label: "Degree",
+        type: "text",
+        name: "degree",
+        placeholder: "Enter degree / Field of study",
+        require: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        label: "Start Date",
+        type: "date",
+        name: "startDate",
+        placeholder: "Enter start date",
+        require: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        label: "End date",
+        type: "date",
+        name: "endDate",
+        placeholder: "Enter End Date",
+        require: false,
+    },
+    {
+        id: crypto.randomUUID(),
+        label: "Location",
+        type: "text",
+        name: "location",
+        placeholder: "Enter Location",
+        require: true,
+    },
+];
+
+export default EducationFormData;
