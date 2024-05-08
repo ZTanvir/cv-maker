ffead5f3b726c6b1ab94c4fbbb180aedfa7df45e
ztanvir <zahirulopel@gmail.com>
2024-05-08 19:23:53 +0600

    Add css for AddInfoButton components

--- src/Components/CvInfo/UserInfo/AddInfoButton.jsx
+++ src/Components/CvInfo/UserInfo/AddInfoButton.jsx
@@ -1,13 +1,17 @@
 /* This is reuseable component
     It will generate a button with a prefered icon and button name
 */
+import styles from "../../../styles/addInfoButton.module.css";
+
 const AddInfoButton = ({ btnIcon, buttonName, handleButton }) => {
     return (
-        <button onClick={handleButton} type="button">
-            {/* add */}
-            <span className="material-symbols-outlined">{btnIcon}</span>
-            <span>{buttonName}</span>
-        </button>
+        <div className={styles.addInfoButtonSection}>
+            <button onClick={handleButton} type="button">
+                {/* add */}
+                <span className="material-symbols-outlined">{btnIcon}</span>
+                <span>{buttonName}</span>
+            </button>
+        </div>
     );
 };
 export default AddInfoButton;
