3770ea03a1f6a132abe37bc3faeef59641e629ed
ztanvir <zahirulopel@gmail.com>
2024-06-10 09:35:41 +0600

    Add SkillRating component that will display user skill level

--- src/Components/CvPreview/DisplaySkills.jsx
+++ src/Components/CvPreview/DisplaySkills.jsx
@@ -1,6 +1,26 @@
+const SkillRating = ({ skillName, level }) => {
+    const maxLevel = [1, 2, 3, 4, 5];
+    return (
+        <div>
+            <p>
+                {skillName}
+                <span>
+                    {maxLevel.map((item) => (
+                        <div key={crypto.randomUUID()}></div>
+                    ))}
+                </span>
+            </p>
+        </div>
+    );
+};
+
 const DisplaySkills = ({ cvData }) => {
     const skills = cvData.skills;
 
-    return <section></section>;
+    return (
+        <section>
+            <h2>Skills</h2>
+        </section>
+    );
 };
 export default DisplaySkills;
