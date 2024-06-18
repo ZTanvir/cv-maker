5dd8443b53e1428e2a8f4ad46400246d1dbb0bc1
zahirul <zahirulopel@gmail.com>
2024-06-18 15:24:20 +0600

    Add flex gap between Contract items

--- src/Components/CvPreview/DisplayContract.jsx
+++ src/Components/CvPreview/DisplayContract.jsx
@@ -1,30 +1,30 @@
 import styles from "../../styles/CvPreview/displayContract.module.css";
 
 const DisplayContract = ({ cvData }) => {
-    const contact = cvData.contract;
+  const contact = cvData.contract;
 
-    return (
-        <section className={styles.contractInfo}>
-            <h2>Contact</h2>
-            {contact.map((item) =>
-                item.visibility === true ? (
-                    <div className={styles.contactList} key={item.id}>
-                        <div>
-                            <p>Phone</p>
-                            <p>{item.phoneNumber}</p>
-                        </div>
-                        <div>
-                            <p>Email</p>
-                            <p>{item.email}</p>
-                        </div>
-                        <div>
-                            <p>Address</p>
-                            <p>{item.address}</p>
-                        </div>
-                    </div>
-                ) : null
-            )}
-        </section>
-    );
+  return (
+    <section className={styles.contractInfo}>
+      <h2>Contact</h2>
+      {contact.map((item) =>
+        item.visibility === true ? (
+          <div className={styles.contactList} key={item.id}>
+            <div className={styles.contactItem}>
+              <p>Phone</p>
+              <p>{item.phoneNumber}</p>
+            </div>
+            <div className={styles.contactItem}>
+              <p>Email</p>
+              <p>{item.email}</p>
+            </div>
+            <div className={styles.contactItem}>
+              <p>Address</p>
+              <p>{item.address}</p>
+            </div>
+          </div>
+        ) : null
+      )}
+    </section>
+  );
 };
 export default DisplayContract;
