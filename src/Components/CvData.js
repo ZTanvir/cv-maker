fe6b67d0465ee10d3a82b84e208ea6e4bab557ed
zahirul <zahirulopel@gmail.com>
2024-06-20 20:33:30 +0600

    Arranged skill and rating as design

--- src/Components/CvData.js
+++ src/Components/CvData.js
@@ -1,60 +1,78 @@
 const CvData = {
-    personal_details: {
-        fname: "Josephine",
-        lname: "Mayers",
-        profession: "Artist",
-        jobSummary:
-            "Engineers, as practitioners of engineering, are professionals who invent, design, analyze, build and test machines, complex systems, structures,",
+  personal_details: {
+    fname: "Josephine",
+    lname: "Mayers",
+    profession: "Artist",
+    jobSummary:
+      "Engineers, as practitioners of engineering, are professionals who invent, design, analyze, build and test machines, complex systems, structures,",
+  },
+  education: [
+    {
+      id: crypto.randomUUID(),
+      institute: "London City University",
+      degree: "H.S.C",
+      startDate: "2014-02-09",
+      endDate: "2014-02-09",
+      location: "",
+      visibility: true,
     },
-    education: [
-        {
-            id: crypto.randomUUID(),
-            institute: "London City University",
-            degree: "H.S.C",
-            startDate: "2014-02-09",
-            endDate: "2014-02-09",
-            location: "",
-            visibility: true,
-        },
-        {
-            id: crypto.randomUUID(),
-            institute: "London City University",
-            degree: "H.S.C",
-            startDate: "2014-02-09",
-            endDate: "2014-02-09",
-            location: "",
-            visibility: true,
-        },
-    ],
-    experience: [
-        {
-            id: crypto.randomUUID(),
-            visibility: true,
-            institute: "Cochran Dawson Plc",
-            designation: "Senior product manager",
-            startDate: "2008-06-23",
-            endDate: "1975-09-21",
-            location: "Consectetur voluptat",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis risus faucibus, ultricies arcu in, gravida metus. Nulla nulla ex, auctor id congue et, eleifend a libero.",
-        },
-    ],
-    contract: [
-        {
-            id: crypto.randomUUID(),
-            visibility: true,
-            phoneNumber: "+1 (195) 436-2106",
-            email: "nohis@mailinator.com",
-            address: "Ut suscipit aliquid",
-        },
-    ],
-    skills: [
-        {
-            id: crypto.randomUUID(),
-            visibility: true,
-            skill: "Reiciendis labore si",
-            level: "4",
-        },
-    ],
+    {
+      id: crypto.randomUUID(),
+      institute: "London City University",
+      degree: "H.S.C",
+      startDate: "2014-02-09",
+      endDate: "2014-02-09",
+      location: "",
+      visibility: true,
+    },
+  ],
+  experience: [
+    {
+      id: crypto.randomUUID(),
+      visibility: true,
+      institute: "Cochran Dawson Plc",
+      designation: "Senior product manager",
+      startDate: "2008-06-23",
+      endDate: "1975-09-21",
+      location: "Consectetur voluptat",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis risus faucibus, ultricies arcu in, gravida metus. Nulla nulla ex, auctor id congue et, eleifend a libero.",
+    },
+  ],
+  contract: [
+    {
+      id: crypto.randomUUID(),
+      visibility: true,
+      phoneNumber: "+1 (195) 436-2106",
+      email: "nohis@mailinator.com",
+      address: "Ut suscipit aliquid",
+    },
+  ],
+  skills: [
+    {
+      id: crypto.randomUUID(),
+      visibility: true,
+      skill: "Photoshop",
+      level: "4",
+    },
+    {
+      id: crypto.randomUUID(),
+      visibility: true,
+      skill: "Indesign",
+      level: "3",
+    },
+    {
+      id: crypto.randomUUID(),
+      visibility: true,
+      skill: "Illustrator",
+      level: "3",
+    },
+    {
+      id: crypto.randomUUID(),
+      visibility: true,
+      skill: "After Effects",
+      level: "2",
+    },
+  ],
 };
 export default CvData;
--- src/Components/CvPreview/DisplaySkills.jsx
+++ src/Components/CvPreview/DisplaySkills.jsx
@@ -1,43 +1,43 @@
 import styles from "../../styles/CvPreview/skillRating.module.css";
 
 const SkillRating = ({ skillName, level, bgColor }) => {
-    const maxLevel = [1, 2, 3, 4, 5];
-    return (
-        <div>
-            <div className={styles.skillSection}>
-                {skillName}
-                <span className={styles.ratingCircles}>
-                    {maxLevel.map((item) => (
-                        <span
-                            key={crypto.randomUUID()}
-                            style={
-                                item <= level
-                                    ? { backgroundColor: "red" }
-                                    : { backgroundColor: "#E1E1E1" }
-                            }
-                            className={styles.ratingCircle}
-                        ></span>
-                    ))}
-                </span>
-            </div>
-        </div>
-    );
+  const maxLevel = [1, 2, 3, 4, 5];
+  return (
+    <div className={styles.skillSection}>
+      <span className={styles.skillName}>{skillName}</span>
+      <span className={styles.ratingCircles}>
+        {maxLevel.map((item) => (
+          <span
+            key={crypto.randomUUID()}
+            style={
+              item <= level
+                ? { backgroundColor: "var(--color-yellow)" }
+                : { backgroundColor: "#E1E1E1" }
+            }
+            className={styles.ratingCircle}
+          ></span>
+        ))}
+      </span>
+    </div>
+  );
 };
 
 const DisplaySkills = ({ cvData }) => {
-    const skills = cvData.skills;
+  const skills = cvData.skills;
 
-    return (
-        <section className={styles.skillsSection}>
-            <h2>Skills</h2>
-            {skills.map((skill) => (
-                <SkillRating
-                    key={crypto.randomUUID()}
-                    skillName={skill.skill}
-                    level={skill.level}
-                />
-            ))}
-        </section>
-    );
+  return (
+    <section className={styles.skillsSection}>
+      <h2>Skills</h2>
+      <div className={styles.skillsRating}>
+        {skills.map((skill) => (
+          <SkillRating
+            key={crypto.randomUUID()}
+            skillName={skill.skill}
+            level={skill.level}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 export default DisplaySkills;
