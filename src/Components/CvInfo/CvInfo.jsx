7295b4c1c60dcb20694659080fb8ab562aa918b9
zahirul <zahirulopel@gmail.com>
2024-07-02 15:53:43 +0600

    feat: show or hide takeing user response section based on click on content,customization

--- src/Components/CvInfo/CvInfo.jsx
+++ src/Components/CvInfo/CvInfo.jsx
@@ -22,8 +22,14 @@ const CvInfo = ({ cvData, setCvData, handleClearBtn, handleLoadBtn }) => {
     // Toggle section
     const [activeIndex, setActiveIndex] = useState(false);
 
-    const handleContent = () => {};
-    const handleCustomization = () => {};
+    const [displaySection, setDisplaySection] = useState("content");
+
+    const handleContent = () => {
+        setDisplaySection("content");
+    };
+    const handleCustomization = () => {
+        setDisplaySection("customization");
+    };
 
     return (
         <section className={styles.cvInFo}>
@@ -35,63 +41,67 @@ const CvInfo = ({ cvData, setCvData, handleClearBtn, handleLoadBtn }) => {
                 handleClearBtn={handleClearBtn}
                 handleLoadBtn={handleLoadBtn}
             />
-            <PersonalDetails cvData={cvData} updateCvData={setCvData} />
-            <InstituteInfo
-                formData={EducationFormData}
-                cvData={cvData}
-                setCvData={setCvData}
-                sectionIcon="school"
-                sectionName="Education"
-                infoSectionTitle="institute"
-                activeIndex={activeIndex === 1}
-                handleToggleSection={(e) =>
-                    activeIndex === 1
-                        ? setActiveIndex(false)
-                        : setActiveIndex(1)
-                }
-            />
-            <InstituteInfo
-                formData={ExperienceFormData}
-                cvData={cvData}
-                setCvData={setCvData}
-                sectionIcon="work"
-                sectionName="Experience"
-                infoSectionTitle="institute"
-                activeIndex={activeIndex === 2}
-                handleToggleSection={(e) =>
-                    activeIndex === 2
-                        ? setActiveIndex(false)
-                        : setActiveIndex(2)
-                }
-            />
-            <InstituteInfo
-                formData={ContractFormData}
-                cvData={cvData}
-                setCvData={setCvData}
-                sectionIcon="contract"
-                sectionName="Contract"
-                infoSectionTitle="email"
-                activeIndex={activeIndex === 3}
-                handleToggleSection={(e) =>
-                    activeIndex === 3
-                        ? setActiveIndex(false)
-                        : setActiveIndex(3)
-                }
-            />
-            <InstituteInfo
-                formData={SkillFormData}
-                cvData={cvData}
-                setCvData={setCvData}
-                sectionIcon="psychology"
-                sectionName="Skills"
-                infoSectionTitle="skill"
-                activeIndex={activeIndex === 4}
-                handleToggleSection={(e) =>
-                    activeIndex === 4
-                        ? setActiveIndex(false)
-                        : setActiveIndex(4)
-                }
-            />
+            {Boolean(displaySection === "content") ? (
+                <div className={styles.cvInFo}>
+                    <PersonalDetails cvData={cvData} updateCvData={setCvData} />
+                    <InstituteInfo
+                        formData={EducationFormData}
+                        cvData={cvData}
+                        setCvData={setCvData}
+                        sectionIcon="school"
+                        sectionName="Education"
+                        infoSectionTitle="institute"
+                        activeIndex={activeIndex === 1}
+                        handleToggleSection={(e) =>
+                            activeIndex === 1
+                                ? setActiveIndex(false)
+                                : setActiveIndex(1)
+                        }
+                    />
+                    <InstituteInfo
+                        formData={ExperienceFormData}
+                        cvData={cvData}
+                        setCvData={setCvData}
+                        sectionIcon="work"
+                        sectionName="Experience"
+                        infoSectionTitle="institute"
+                        activeIndex={activeIndex === 2}
+                        handleToggleSection={(e) =>
+                            activeIndex === 2
+                                ? setActiveIndex(false)
+                                : setActiveIndex(2)
+                        }
+                    />
+                    <InstituteInfo
+                        formData={ContractFormData}
+                        cvData={cvData}
+                        setCvData={setCvData}
+                        sectionIcon="contract"
+                        sectionName="Contract"
+                        infoSectionTitle="email"
+                        activeIndex={activeIndex === 3}
+                        handleToggleSection={(e) =>
+                            activeIndex === 3
+                                ? setActiveIndex(false)
+                                : setActiveIndex(3)
+                        }
+                    />
+                    <InstituteInfo
+                        formData={SkillFormData}
+                        cvData={cvData}
+                        setCvData={setCvData}
+                        sectionIcon="psychology"
+                        sectionName="Skills"
+                        infoSectionTitle="skill"
+                        activeIndex={activeIndex === 4}
+                        handleToggleSection={(e) =>
+                            activeIndex === 4
+                                ? setActiveIndex(false)
+                                : setActiveIndex(4)
+                        }
+                    />
+                </div>
+            ) : null}
         </section>
     );
 };
