5ad96bb06d9c18891a08d2eff0689b903a04088f
zahirul <zahirulopel@gmail.com>
2024-07-03 15:19:17 +0600

    font: Add font Roboto, Merriweather, Fira Sans from google link

---
+++ src/Components/CvInfo/CustomizeCv/CvFonts.jsx
@@ -0,0 +1,22 @@
+const CvFonts = () => {
+    return (
+        <section>
+            <h2>Fonts</h2>
+            <div className="">
+                <button className="">
+                    <span>Aa</span>
+                    <span>Serif</span>
+                </button>
+                <button>
+                    <span></span>
+                    <span></span>
+                </button>
+                <button>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+        </section>
+    );
+};
+export default CvFonts;
