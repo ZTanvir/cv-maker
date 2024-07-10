923abd00eecae37e9197824d6717e62966de365c
zahirul <zahirulopel@gmail.com>
2024-07-10 15:29:33 +0600

    bug: on empty education end date front end will show Ongoing

--- src/Components/CvPreview/DisplayEduInstitute.jsx
+++ src/Components/CvPreview/DisplayEduInstitute.jsx
@@ -1,14 +1,14 @@
 const DisplayEduInstitute = ({ insituteInfo }) => {
     const formateDate = (date) => date.split("-").join("/");
     const startDate = formateDate(insituteInfo.startDate);
-    const endDate = formateDate(insituteInfo.startDate);
+    const endDate = formateDate(insituteInfo.endDate);
 
     return (
         <section>
             {insituteInfo.visibility ? (
                 <>
                     <p>
-                        {startDate} - {endDate}
+                        {startDate} - {endDate ? endDate : "Ongoing"}
                     </p>
                     <p>{insituteInfo.degree}</p>
                     <p>{insituteInfo.institute}</p>
